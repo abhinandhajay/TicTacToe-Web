b47d88034f48c75ea7b7a5221a3bd6ef70cc4d08
Abhinandh Ajay <abhinandhajay@gmail.com>
2019-10-04 10:13:52 +0530

    Fix bug with multiple clicks

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ var turn = 1;
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
-        if (turn % 2 === 1) {
-            this.classList.add("squareX");
-            this.classList.remove("square");
-            this.innerHTML = "<i class='fas fa-times'></i>";
-            h1.style.background = "#39da6f";
-            turn++;
-        } else {
-            this.classList.add("squareO");
-            this.classList.remove("square");
-            this.innerHTML = "<i class='far fa-circle'></i>";
-            h1.style.background = "#df3535";
-            turn++;
+        if (this.classList.contains("square")) {
+            if (turn % 2 === 1) {
+                this.classList.add("squareX");
+                this.classList.remove("square");
+                this.innerHTML = "<i class='fas fa-times'></i>";
+                h1.style.background = "#39da6f";
+                turn++;
+            } else {
+                this.classList.add("squareO");
+                this.classList.remove("square");
+                this.innerHTML = "<i class='far fa-circle'></i>";
+                h1.style.background = "#df3535";
+                turn++;
+            }
         }
     });
 }
