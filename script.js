5f01160a2c87403d28b08854e4f7c90e67fee0a3
Abhinandh Ajay <abhinandhajay@gmail.com>
2019-10-04 15:10:02 +0530

    Added winner rules for rows

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 var squares = document.querySelectorAll(".square");
 var h1 = document.querySelector("h1");
+var one = document.querySelector("#one");
+var two = document.querySelector("#two");
+var three = document.querySelector("#three");
+var four = document.querySelector("#four");
+var five = document.querySelector("#five");
+var six = document.querySelector("#six");
+var seven = document.querySelector("#seven");
+var eight = document.querySelector("#eight");
+var nine = document.querySelector("#nine");
 var turn = 1;
+var end = 0;
+
+function checker() {
+    if ((one.className == two.className) && (one.className == three.className)) {
+        if (one.className == "squareX") {
+            end = 1;
+            alert("Player 1");
+            return "1";
+        } else if (one.className == "squareO") {
+            end = 1;
+            alert("Player 2");
+            return "2";
+        }
+    }
+    if ((four.className == five.className) && (four.className == six.className)) {
+        if (four.className == "squareX") {
+            end = 1;
+            alert("Player 1");
+            return "1";
+        } else if (four.className == "squareO") {
+            end = 1;
+            alert("Player 2");
+            return "2";
+        }
+    }
+    if ((seven.className == eight.className) && (seven.className == nine.className)) {
+        if (seven.className == "squareX") {
+            end = 1;
+            alert("Player 1");
+            return "1";
+        } else if (seven.className == "squareO") {
+            end = 1;
+            alert("Player 2");
+            return "2";
+        }
+    }
+}
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
-        if (this.classList.contains("square")) {
+        if (this.classList.contains("square") && end == 0) {
             if (turn % 2 === 1) {
                 this.classList.add("squareX");
                 this.classList.remove("square");
@@ -18,6 +64,7 @@ for (var i = 0; i < squares.length; i++) {
                 h1.style.background = "#df3535";
                 turn++;
             }
+            checker();
         }
     });
 }
